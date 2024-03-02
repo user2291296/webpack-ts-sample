fe0618e41b142262483f36aa3c10b4e26fb79a7c
Dmitry Zernov <dzernov@luxms.com>
2024-03-02 12:32:25 +0300

    Initial commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+            outputPath: 'img/',
+            name: '[name].[ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        favicon: './src/favicon.png'
+    }),
+  ],
+};
